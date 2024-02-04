38f4960f43d7d8a989a6044e11f962e277ac813b
GregorJUofG <2773209j@student.gla.ac.uk>
2024-02-04 18:40:27 +0000

    Chapter 8

--- rango/views.py
+++ rango/views.py
@@ -9,18 +9,15 @@ from django.urls import reverse
 
 
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by the number of likes in descending order.
-    # Retrieve the top 5 only -- or all if less than 5.
-    # Place the list in our context_dict dictionary (with our boldmessage!)
-    # that will be passed to the template engine.
+    # The '-' in likes means descedning order
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
+
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    # Render the response and send it back!
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
@@ -28,8 +25,6 @@ def about(request):
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
-    # Create a context dictionary which we can pass
-    # to the template rendering engine.
     context_dict = {}
     try:
         # Can we find a category name slug with the given name?
